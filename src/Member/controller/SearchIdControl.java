bb167a7b115b8d78cd9b7ff4ca282e42ac81d5c2
soon0824 <mpqwe24@gmail.com>
2021-05-11 12:04:50 +0900

    아이디/비번 찾기 실패페이지추가

--- src/Member/controller/SearchIdControl.java
+++ src/Member/controller/SearchIdControl.java
@@ -30,38 +30,36 @@ public class SearchIdControl extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 1. 인코딩
-		request.setCharacterEncoding("UTF-8");
-		
-		// 2. 변수저장
-		String name = request.getParameter("name");
-		String email = request.getParameter("email");
-
-		// 3. 비지니스로직
-		MemberDAO dao = new MemberDAO();
-		Member member = dao.searchId(name, email); 
-	
-		System.out.println("member : " + member);
-		
-		// 4. 뷰 처리
-		if(member != null) {
-			RequestDispatcher rd = request.getRequestDispatcher("/member/IDverification.jsp");
-			request.setAttribute("members", member);
-			rd.forward(request, response);
-		} else {
-			request.setAttribute("msg", "정확한 정보를 입력해주세요!");
-			request.setAttribute("loc", "/");	
-			RequestDispatcher rd = request.getRequestDispatcher("");
-			rd.forward(request, response);
-		
-		}
-		
+		doPost(request, response);
 	}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request,response);
-	}
+		
+		// 1. 인코딩
+				request.setCharacterEncoding("UTF-8");
+				// 2. 변수저장
+				String name = request.getParameter("name");
+				String email = request.getParameter("email");
 
+				// 3. 비지니스로직
+				MemberDAO dao = new MemberDAO();
+				Member member = dao.searchId(name, email); 
+			
+				System.out.println("member : " + member);
+				
+				// 4. 뷰 처리
+				if(member != null) {
+					RequestDispatcher rd = request.getRequestDispatcher("/member/IDverification.jsp");
+					request.setAttribute("members", member);
+					rd.forward(request, response);
+				} else {
+					request.setAttribute("msg", "정확한 정보를 입력해주세요!");
+					request.setAttribute("loc", "/");	
+					RequestDispatcher rd = request.getRequestDispatcher("/member/searchIdFail.jsp");
+					rd.forward(request, response);
+				
+				}
+	}
 }
